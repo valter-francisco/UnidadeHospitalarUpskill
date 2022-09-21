e7f87b6cd75fc7b609b23cae24bb8f78cfcdae31
Valter Francisco <valter.francisco@students.assembly.pt>
2022-09-21 12:17:05 +0100

    new

---
+++ src/main/java/com/uhugrupob/unidadehospitalarupskill/UnidadeHospitalarUpskillApplication.java
@@ -0,0 +1,13 @@
+package com.uhugrupob.unidadehospitalarupskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidadeHospitalarUpskillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnidadeHospitalarUpskillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uhugrupob/unidadehospitalarupskill/UnidadeHospitalarUpskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhugrupob.unidadehospitalarupskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidadeHospitalarUpskillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
